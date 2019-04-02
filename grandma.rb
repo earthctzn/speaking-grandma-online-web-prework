3095a206ccc432e7f41722acc6e4d98e4cfe6def
Caleb Aviles <earthctzn1@gmail.com>
2019-04-02 02:57:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,12 @@
+def speak_to_grandma(x)
+  x="I love you grandma!"
+  if x == x.upcase
+    return "I LOVE YOU TOO PUMPKIN!"
+elseif 
+  return "NO, NOT SINCE 1938" if x == x
+  else return "HUH?! SPEAK UP, SONNY!" if x != x.upcase
+    end
+    
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with

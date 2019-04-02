8bebb4fd1f68f68abeafc1f79e31dbe1f8f30449
Caleb Aviles <earthctzn1@gmail.com>
2019-04-02 03:33:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA"
     return "I LOVE YOU TOO PUMPKIN!"
+  elseif
 end
 end #  elseif phrase != phrase.upcase
 #  return "HUH?! SPEAK UP, SONNY!"
